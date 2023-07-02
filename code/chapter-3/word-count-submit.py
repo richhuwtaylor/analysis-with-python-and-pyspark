63b22df24efaea28e3fd0c1dbb4035ca39585ccb
Rich Taylor <rich@taylor.it>
2023-07-02 15:47:21 +0100

    Add chapter 3 solutions

---
+++ code/chapter-3/word-count-submit.py
@@ -0,0 +1,14 @@
+import pyspark.sql.functions as F
+
+results = (
+    spark.read.text("../data/gutenberg_books/1342-0.txt")
+    .select(F.split(F.col("value"), " ").alias("line"))
+    .select(F.explode(F.col("line")).alias("word"))
+    .select(F.lower(F.col("word")).alias("word"))
+    .select(F.regexp_extract(F.col("word"), "[a-z']*", 0).alias("word"))
+    .where(F.col("word") != "")
+    .groupby("word")
+    .count()
+)
+
+results.show()
